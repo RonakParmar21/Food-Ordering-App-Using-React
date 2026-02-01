ed53ec8a5da055c2817b2fb7a03fcc3fb97cc8e9
Ronak Parmar <parmarronak21112003@gmail.com>
2026-02-01 17:52:43 +0530

    shimmer ui css add

--- food-ordering/src/components/Shimmer.jsx
+++ food-ordering/src/components/Shimmer.jsx
@@ -1,23 +1,21 @@
 const Shimmer = () => {
   return (
     <div className="shimmer-container">
-      {Array(5)
-        .fill("")
-        .map((_, i) => (
-          <div className="shimmer-cards" key={i}>
-            <div className="shimmer shimmer-img"></div>
+      {Array(5).fill("").map((_, i) => (
+        <div className="shimmer-cards" key={i}>
+          <div className="shimmer shimmer-img"></div>
 
-            <div className="shimmer-content">
-              <div className="shimmer shimmer-title"></div>
-              <div className="shimmer shimmer-text"></div>
+          <div className="shimmer-content">
+            <div className="shimmer shimmer-title"></div>
+            <div className="shimmer shimmer-text"></div>
 
-              <div className="shimmer-meta">
-                <div className="shimmer shimmer-badge"></div>
-                <div className="shimmer shimmer-time"></div>
-              </div>
+            <div className="shimmer-meta">
+              <div className="shimmer shimmer-badge"></div>
+              <div className="shimmer shimmer-time"></div>
             </div>
           </div>
-        ))}
+        </div>
+      ))}
     </div>
   );
 };
